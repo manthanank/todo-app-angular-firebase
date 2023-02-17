278efea04ce859ae12a2e9a3adfa55c7e7116d42
Manthan Ankolekar <manthan.ank@gmail.com>
2023-02-17 23:51:10 +0530

    updated

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'todo-app-angular'`, () => {
+  it(`should have as title 'todo-app-angular-firebase'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('todo-app-angular');
+    expect(app.title).toEqual('todo-app-angular-firebase');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('todo-app-angular app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('todo-app-angular-firebase app is running!');
   });
 });
