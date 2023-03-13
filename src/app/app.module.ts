a0bfac0ef4cc50c72bbd63b3ac513eab501fca60
Manthan Ankolekar <manthan.ank@gmail.com>
2023-03-13 09:16:01 +0530

    app updated to latest

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,6 @@ import { environment } from '../environments/environment';
 import { HttpClientModule } from '@angular/common/http';
 import { TodoComponent } from './todo/todo.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
-import { provideDatabase,getDatabase } from '@angular/fire/database';
 import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
 import { AngularFireModule } from '@angular/fire/compat';
 import {MatIconModule} from '@angular/material/icon';
@@ -23,8 +21,6 @@ import {MatIconModule} from '@angular/material/icon';
     HttpClientModule,
     TodoComponent,
     ReactiveFormsModule,
-    // provideFirebaseApp(() => initializeApp(environment.firebase)),
-    // provideDatabase(() => getDatabase())
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
     MatIconModule
