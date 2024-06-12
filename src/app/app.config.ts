2cfd3795a61225dade39da13e6b0c1d013f07e9f
Manthan Ankolekar <manthan.ank46@gmail.com>
2024-06-12 15:49:14 +0530

    chore: Remove unused files and update dependencies

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,27 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes), 
-    provideClientHydration(), 
-    importProvidersFrom(provideFirebaseApp(() => initializeApp({ "projectId": "todo-app-angular-firebase", "appId": "1:457747990162:web:5ae72a3c09ba41965d484b", "databaseURL": "https://todo-app-angular-firebase-default-rtdb.firebaseio.com", "storageBucket": "todo-app-angular-firebase.appspot.com", "apiKey": "AIzaSyAq5203yUdYZpgJlc9mi2A1RA03Qp7xSf0", "authDomain": "todo-app-angular-firebase.firebaseapp.com", "messagingSenderId": "457747990162", "measurementId": "G-MVLYWBKZCF" }))), 
-    importProvidersFrom(provideFirestore(() => getFirestore()))]
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() =>
+      initializeApp({
+        apiKey: 'AIzaSyAq5203yUdYZpgJlc9mi2A1RA03Qp7xSf0',
+        authDomain: 'todo-app-angular-firebase.firebaseapp.com',
+        databaseURL:
+          'https://todo-app-angular-firebase-default-rtdb.firebaseio.com',
+        projectId: 'todo-app-angular-firebase',
+        storageBucket: 'todo-app-angular-firebase.appspot.com',
+        messagingSenderId: '457747990162',
+        appId: '1:457747990162:web:5ae72a3c09ba41965d484b',
+        measurementId: 'G-MVLYWBKZCF',
+      })
+    ),
+    provideFirestore(() => getFirestore()),
+  ],
 };
